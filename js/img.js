cf6b2e8ead4e1d4bc5546078e570cf635700297c
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2018-03-24 11:08:27 +0900

    Refine img.js

--- js/img.js
+++ js/img.js
@@ -1,37 +1,56 @@
 const showImg = () => {
+
     const imgsInsideLinks = [];
+    const imgsWithoutLinks = [];
     const links = document.getElementsByTagName('a');
+    const imgs = document.getElementsByTagName('img');
+
     for (const link of links) {
         const imgsInsideLink = link.getElementsByTagName('img');
         for (const imgInsideLink of imgsInsideLink) {
             imgsInsideLinks.push(imgInsideLink);
         }
     }
-    console.log(imgsInsideLinks);
-    const imgs = document.getElementsByTagName('img');
-    for (let i = 0; i < imgs.length; i++) {
-        if (imgsInsideLinks.indexOf(imgs[i]) >= 0) {
-            imgs[i] = '';
+    for (const img of imgs) {
+        if (!imgsInsideLinks.includes(img)) {
+            imgsWithoutLinks.push(img);
         }
     }
-    console.log(imgs);
-    for (img of imgs) {
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+        #img-overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100vh;
+            background: rgba(0, 0, 0, 0.8);
+            background-size: contain;
+            background-position: center;
+            background-repeat: no-repeat;
+            opacity: 0;
+            transition: opacity 200ms;
+        }
+        #img-overlay.show {
+            opacity: 1;
+        }
+    `;
+    document.body.appendChild(style);
+
+    for (const img of imgsWithoutLinks) {
         img.addEventListener('click', (e) => {
+
             const overlay = document.createElement('div');
-            overlay.id = 'overlay';
-            overlay.style.position = 'fixed';
-            overlay.style.top = 0;
-            overlay.style.left = 0;
-            overlay.style.width = '100%';
-            overlay.style.height = '100vh';
-            overlay.style.background = 'rgba(255, 255, 255, 0.9)';
+            overlay.id = 'img-overlay';
             overlay.style.backgroundImage = `url(${e.target.getAttribute('src')})`;
-            overlay.style.backgroundSize = 'contain';
-            overlay.style.backgroundPosition = 'center';
-            overlay.style.backgroundRepeat = 'no-repeat';
             document.body.style.overflow = 'hidden';
             document.body.style.height = '100vh';
             document.body.appendChild(overlay);
+
+            setTimeout(() => {
+                overlay.classList.toggle('show');
+            }, 5);
 
             overlay.addEventListener('click', (e) => {
                 e.target.remove();
